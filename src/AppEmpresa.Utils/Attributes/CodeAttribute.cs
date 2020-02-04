3096e5556f755c1a850f8110a16d1d84a4cf8af1
Eddersouza <eddyesouza@gmail.com>
2020-02-04 20:23:02 -0300

    feat - criação de DataAnnotation para código e extensão para pega-lo

---
+++ src/AppEmpresa.Utils/Attributes/CodeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AppEmpresa.Utils.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class CodeAttribute : Attribute
+    {
+        public CodeAttribute(string code)
+        {
+            CodeValue = code;
+        }
+
+        public virtual string Code { get { return CodeValue; } }
+
+        protected string CodeValue { get; set; }
+    }
+}
--- src/AppEmpresa.Utils/Extensions/Enums.cs
+++ src/AppEmpresa.Utils/Extensions/Enums.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using AppEmpresa.Utils.Attributes;
+using System;
 using System.ComponentModel;
 
 namespace AppEmpresa.Utils.Extensions
 {
     public static class Enums
     {
+        public static string GetCode(this Enum value)
+        {
+            var attribute = value.GetAttribute<CodeAttribute>();
+            return attribute == null ? string.Empty : attribute.Code;
+        }
+
+        public static string GetDescription(this Enum value)
+        {
+            var attribute = value.GetAttribute<DescriptionAttribute>();
+            return attribute == null ? string.Empty : attribute.Description;
+        }
+
         private static T GetAttribute<T>(this Enum value) where T : Attribute
         {
             var type = value.GetType();
@@ -13,12 +26,6 @@ namespace AppEmpresa.Utils.Extensions
             return attributes.Length > 0
               ? (T)attributes[0]
               : null;
-        }
-
-        public static string GetDescription(this Enum value)
-        {
-            var attribute = value.GetAttribute<DescriptionAttribute>();
-            return attribute == null ? string.Empty : attribute.Description;
         }
     }
 }
