7e2f3955796cae98a6ba715b5520fcb5fcad9e98
Eddersouza <eddyesouza@gmail.com>
2020-02-04 20:58:21 -0300

    feat - criado entidade "Empresas" e "Lista de Empresas"

---
+++ src/AppEmpresa.Domain/Entities/Company.cs
@@ -0,0 +1,21 @@
+﻿using AppEmpresa.Domain.Enums;
+
+namespace AppEmpresa.Domain.Entities
+{
+    public class Company
+    {
+        public Company(
+            string cnpj,
+            string companyName,
+            State state)
+        {
+            CNPJ = cnpj;
+            CompanyName = companyName;
+            State = state;
+        }
+
+        public string CNPJ { get; set; }
+        public string CompanyName { get; set; }
+        public State State { get; set; }
+    }
+}
---
+++ src/AppEmpresa.Domain/Entities/CompanyList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace AppEmpresa.Domain.Entities
+{
+    public class CompanyList
+    {
+        private ICollection<Company> _itens;
+
+        public CompanyList()
+        {
+            _itens = new List<Company>();
+        }
+
+        public CompanyList(
+            ICollection<Company> itens)
+        {
+            _itens = itens;
+        }
+
+        public ICollection<Company> Itens { get { return _itens; } }
+    }
+}
