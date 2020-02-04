11b3c3e08aecef5450b4e320165ae9634e39eda1
Eddersouza <eddyesouza@gmail.com>
2020-02-03 22:24:36 -0300

    feat Criação do Enumerador de Estados

---
+++ src/AppEmpresa.Domain/Enums/State.cs
@@ -0,0 +1,115 @@
+﻿using System.ComponentModel;
+
+namespace AppEmpresa.Domain.Enums
+{
+    public enum State
+    {
+        //AC
+        [Description("Acre")]
+        Acre,
+
+        //AL
+        [Description("Alagoas")]
+        Alagoas,
+
+        //AP
+        [Description("Amapá")]
+        Amapa,
+
+        //AM
+        [Description("Amazonas")]
+        Amazonas,
+
+        //BA
+        [Description("Bahia")]
+        Bahia,
+
+        //CE
+        [Description("Ceará")]
+        Ceara,
+
+        //ES
+        [Description("Espírito Santo")]
+        EspiritoSanto,
+
+        //GO
+        [Description("Goiás")]
+        Goias,
+
+        //MA
+        [Description("Maranhão")]
+        Maranhao,
+
+        //MT
+        [Description("Mato Grosso")]
+        MatoGrosso,
+
+        //MS
+        [Description("Mato Grosso do Sul")]
+        MatoGrossoDoSul,
+
+        //MG
+        [Description("Minas Gerais")]
+        MinasGerais,
+
+        //PA
+        [Description("Pará")]
+        Para,
+
+        //PB
+        [Description("Paraíba")]
+        Paraiba,
+
+        //PR
+        [Description("Paraná")]
+        Parana,
+
+        //PE
+        [Description("Pernambuco")]
+        Pernambuco,
+
+        //PI
+        [Description("Piauí")]
+        Piaui,
+
+        //RJ
+        [Description("Rio de Janeiro")]
+        RioDeJaneiro,
+
+        //RN
+        [Description("Rio Grande do Norte")]
+        RioGrandeDoNorte,
+
+        //RS
+        [Description("Rio Grande do Sul")]
+        RioGrandeDoSul,
+
+        //RO
+        [Description("Rondônia")]
+        Rondonia,
+
+        //RR
+        [Description("Roraima")]
+        Roraima,
+
+        //SC
+        [Description("Santa Catarina")]
+        SantaCatarina,
+
+        //SP
+        [Description("São Paulo")]
+        SaoPaulo,
+
+        //SE
+        [Description("Sergipe")]
+        Sergipe,
+
+        //TO
+        [Description("Tocantins")]
+        Tocantins,
+
+        //DF
+        [Description("Distrito Federal")]
+        DistritoFederal
+    }
+}
