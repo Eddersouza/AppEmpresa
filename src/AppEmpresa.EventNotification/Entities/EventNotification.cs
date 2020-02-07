7aee8910e0a82b0b830e1d939555fbc855dc1211
eddersouza <eddye_souza@yahoo.com.br>
2020-02-06 21:46:55 -0300

    feat - adicionado event notiication

---
+++ src/AppEmpresa.EventNotification/Entities/EventNotification.cs
@@ -0,0 +1,34 @@
+﻿using AppEmpresa.EventNotification.Base;
+using AppEmpresa.EventNotification.Entities.Levels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppEmpresa.EventNotification.Entities
+{
+    public class EventNotification : EventNotificationBase
+    {
+        public IList<EventNotificationDescription> Errors =>
+            List.Cast<EventNotificationDescription>()
+            .Where(x => x.Level is EventNotificationError).ToList();
+
+        public bool HasErrors =>
+           List.Cast<EventNotificationDescription>()
+           .Any(x => x.Level is EventNotificationError);
+
+        public bool HasInformations =>
+            List.Cast<EventNotificationDescription>()
+            .Any(x => x.Level is EventNotificationInformation);
+
+        public bool HasWarnings =>
+            List.Cast<EventNotificationDescription>()
+            .Any(x => x.Level is EventNotificationWarning);
+
+        public IList<EventNotificationDescription> Informations =>
+            List.Cast<EventNotificationDescription>()
+            .Where(x => x.Level is EventNotificationInformation).ToList();
+
+        public IList<EventNotificationDescription> Warnings =>
+           List.Cast<EventNotificationDescription>()
+           .Where(x => x.Level is EventNotificationWarning).ToList();
+    }
+}
---
+++ src/AppEmpresa.EventNotification/Entities/EventNotificationDescription.cs
@@ -0,0 +1,19 @@
+﻿using AppEmpresa.EventNotification.Base;
+using AppEmpresa.EventNotification.Contracts;
+
+namespace AppEmpresa.EventNotification.Entities
+{
+    public class EventNotificationDescription : EventNotificationDescriptionBase
+    {
+        public EventNotificationDescription(
+             string message,
+             EventNotificationLevelContract level,
+             params string[] args)
+             : base(message, args)
+        {
+            Level = level;
+        }
+
+        public EventNotificationLevelContract Level { get; }
+    }
+}
