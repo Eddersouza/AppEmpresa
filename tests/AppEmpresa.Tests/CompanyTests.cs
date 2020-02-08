f8abcdcfb468bc794087e66f55eab3cbc07d30d3
eddersouza <eddye_souza@yahoo.com.br>
2020-02-08 17:47:08 -0300

    teste - Criação de teste para criação de empresa com sucesso

--- tests/AppEmpresa.Tests/CompanyTests.cs
+++ tests/AppEmpresa.Tests/CompanyTests.cs
@@ -1,6 +1,25 @@
-﻿namespace AppEmpresa.Tests
+﻿using AppEmpresa.App.Services;
+using AppEmpresa.Domain.Contracts.Apps;
+using AppEmpresa.Domain.Entities;
+using AppEmpresa.Domain.Enums;
+using NUnit.Framework;
+
+namespace AppEmpresa.Tests
 {
     public class CompanyTests
     {
+        [Test]
+        public void CreateCompany()
+        {
+            //Arrange
+            CompanyAppContract _companyApp = new CompanyApp();
+            Company company = new Company("10793548000190", "Company Name", State.Acre);
+
+            //Act
+            company = _companyApp.Create(company);
+
+            //Assert
+            Assert.AreEqual(true, company.IsValid());
+        }
     }
 }
