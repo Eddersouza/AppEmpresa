52bf10ec0d71aa6560546bbf95d4c9a499e815da
eddersouza <eddye_souza@yahoo.com.br>
2020-02-07 22:38:08 -0300

    feat - criado app contract para Company

---
+++ src/AppEmpresa.Domain/Contracts/Apps/CompanyAppContract.cs
@@ -0,0 +1,15 @@
+﻿using AppEmpresa.Domain.Entities;
+
+namespace AppEmpresa.Domain.Contracts.Apps
+{
+    public interface CompanyAppContract
+    {
+        Company Create(Company company);
+
+        Company Delete(Company company);
+
+        CompanyList Get();
+
+        Company Update(Company company);
+    }
+}
