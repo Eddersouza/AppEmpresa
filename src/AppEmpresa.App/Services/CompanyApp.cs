5cdb33f68726829bdd923bc46fb37f898d3761b4
eddersouza <eddye_souza@yahoo.com.br>
2020-02-08 14:22:16 -0300

    feat - Criado app service para "Empresa"

---
+++ src/AppEmpresa.App/Services/CompanyApp.cs
@@ -0,0 +1,29 @@
+﻿using AppEmpresa.Domain.Contracts.Apps;
+using AppEmpresa.Domain.Entities;
+using System;
+
+namespace AppEmpresa.App.Services
+{
+    public class CompanyApp : CompanyAppContract
+    {
+        public Company Create(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Delete(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompanyList Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Update(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
