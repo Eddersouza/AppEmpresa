d704ef48f6b2679185ecf8860c1a649ebb751dd2
eddersouza <eddye_souza@yahoo.com.br>
2020-02-08 17:48:10 -0300

    feat -  Criação de empresa com cnpj errado

--- tests/AppEmpresa.Tests/CompanyTests.cs
+++ tests/AppEmpresa.Tests/CompanyTests.cs
@@ -3,6 +3,7 @@ using AppEmpresa.Domain.Contracts.Apps;
 using AppEmpresa.Domain.Entities;
 using AppEmpresa.Domain.Enums;
 using NUnit.Framework;
+using System.Linq;
 
 namespace AppEmpresa.Tests
 {
@@ -20,6 +21,21 @@ namespace AppEmpresa.Tests
 
             //Assert
             Assert.AreEqual(true, company.IsValid());
+        }
+
+
+        [Test]
+        public void CreateCompany_CNPJ_Wrong()
+        {
+            //Arrange
+            CompanyAppContract _companyApp = new CompanyApp();
+            Company company = new Company("10793548000190", "Company Name", State.Acre);
+
+            //Act
+            company = _companyApp.Create(company);
+
+            //Assert
+            Assert.AreEqual(true, company.EventNotification.Errors.Select(x => x.ToString().Contains("CNPJ Inválido.")));
         }
     }
 }
