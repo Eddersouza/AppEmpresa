29980720dc71fe08bfc1c7cdaca2541379355f90
eddersouza <eddye_souza@yahoo.com.br>
2020-02-08 21:08:57 -0300

    feat - ajuste repositorio de Empresa

--- src/AppEmpresa.Domain/Contracts/Repositories/Base/UnityOfWorkContract.cs
+++ src/AppEmpresa.Domain/Contracts/Repositories/Base/UnityOfWorkContract.cs
@@ -4,6 +4,8 @@ namespace AppEmpresa.Domain.Contracts.Repositories.Base
 {
     public interface UnityOfWorkContract
     {
+        CompanyRepositoryContract companyRepository { get; }
+
         Task BeginTransaction();
 
         Task Commit();
