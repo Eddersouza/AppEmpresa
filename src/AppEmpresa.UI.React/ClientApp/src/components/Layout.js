7b5e327339a80bdd24fb7bbb29871e3fe90f750a
eddersouza <eddye_souza@yahoo.com.br>
2020-03-01 12:55:07 -0300

    feat link para as paginas Empresa e pagina inicial no header

--- src/AppEmpresa.UI.React/ClientApp/src/components/Layout.js
+++ src/AppEmpresa.UI.React/ClientApp/src/components/Layout.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Container from '@material-ui/core/Container';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,21 +14,52 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
   },
+  menu: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+    padding: '15px'
+  },
+  link: {
+    color: '#fff',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  }
 }));
 
 function LayoutApp(props) {
   const classes = useStyles();
 
+  const preventDefault = event => event.preventDefault();
   return (
     <>
       <div className={classes.root}>
         <AppBar position="static">
-          <Toolbar>            
+          <Toolbar>
             <Typography variant="h6" className={classes.title}>
-              App Empresa
-          </Typography>
+              <Link to='/' className={classes.link} >
+                App Empresa
+              </Link>
+            </Typography>
+            <div className={classes.menu}>
+              <Typography >
+                <Link to='/empresas' className={classes.link} >
+                  Empresas
+              </Link>
+              </Typography>
+            </div>
           </Toolbar>
         </AppBar>
       </div>
